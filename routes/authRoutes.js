6e5198cdcfb1970b2efcbc8da9d9cbe3c8988e9b
Fortexfreddie <106601096+Fortexfreddie@users.noreply.github.com>
2025-09-24 21:21:28 +0100

    chore: comment out profile route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,9 +9,9 @@ router.post('/signup', signup);  // Route for user signup
 router.post('/login', login);    // Route for user login
 
 // Example of a protected route (only accessible with valid token)
-router.get('/profile', authMiddleware, (req, res) => {
-  res.json({ message: `Welcome, user ${req.user.id}` }); 
-});
+// router.get('/profile', authMiddleware, (req, res) => {
+//   res.json({ message: `Welcome, user ${req.user.id}` }); 
+// });
 
 // Export router to be used in server.js
 export default router;
